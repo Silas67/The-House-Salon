d74d6512e483feb134f75923f9063a08e6547c64
Silas67 <chibukesilas@gmail.com>
2025-10-11 04:23:03 +0100

    Commit 1.2

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-export default async function Page({ params }: { params: { id: string } }) {
+interface BlogPageProps {
+  params: { id: string };
+}
+
+export default async function Page({ params }: BlogPageProps) {
   const { id } = params;
 
   return (
     <div>
-      {/* Hero */}
       <div className="min-h-[20vh] w-full text-white p-8 bg-primary space-y-28">
         <div className="max-w-4xl flex items-start flex-col mt-28 Stardom-font gap-2">
           <h1 className="text-6xl max-sm:text-4xl">{id}</h1>
@@ -14,4 +17,3 @@ export default async function Page({ params }: { params: { id: string } }) {
     </div>
   );
 }
-
