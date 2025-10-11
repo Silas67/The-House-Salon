7acafaf1680c4ecb20aa5d27da022f28d39b3b09
Silas67 <chibukesilas@gmail.com>
2025-10-11 04:19:48 +0100

    Commit 1.1

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
 
-export default async function page({ params }: { params: { id: string } }) {
-  const { id } = await params;
   return (
     <div>
       {/* Hero */}
-      <div className="min-h-[20vh] w-full text-white  p-8 bg-primary space-y-28">
+      <div className="min-h-[20vh] w-full text-white p-8 bg-primary space-y-28">
         <div className="max-w-4xl flex items-start flex-col mt-28 Stardom-font gap-2">
           <h1 className="text-6xl max-sm:text-4xl">{id}</h1>
         </div>
@@ -14,3 +14,4 @@ export default async function page({ params }: { params: { id: string } }) {
     </div>
   );
 }
+
